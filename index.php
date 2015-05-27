bebd445910f057fc977bb1a020a80bac1faa97c7
Ismael Valenzuela <iavalenzu@gmail.com>
2015-05-27 00:40:45 -0300

    First Commit!!

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+	$array = array(
+	    "Leaf1" => rand(0, 20),
+	    "Leaf2" => rand(0, 20),
+	    "Leaf3" => rand(0, 20),
+	    "Leaf4" => rand(0, 20),
+	    "Leaf5" => rand(0, 20),
+	    "Leaf6" => rand(0, 20),
+	    "Leaf7" => rand(0, 20),
+	    "Leaf8" => rand(0, 20)
+	);
+
+	echo json_encode($array);
+
+?>
